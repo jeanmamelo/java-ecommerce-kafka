d9ccdcbec9710291d4ada3f3c69e1cd0d647315a
jeanmamelo <jeanmamelo@gmail.com>
2022-02-14 00:51:18 -0300

    Create monorepo and separate dependencies

--- src/main/java/br/com/jeanmamelo/ecommerce/ConsumerFunction.java
+++
@@ -1,7 +0,0 @@
-package br.com.jeanmamelo.ecommerce;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface ConsumerFunction {
-    void consume(ConsumerRecord<String, String> record);
-}
